ba94685c32ee5160d5fc5973bd7405845b91d329
Zohaib <zohaibv1999@gmail.com>
2025-08-01 23:01:29 -0400

    2.4

--- Part_2/courseinfo/src/App.jsx
+++ Part_2/courseinfo/src/App.jsx
@@ -1,7 +1,7 @@
 let todoCounter = 1;
 
 
-const Header = (props) => <h1>{props.course}</h1>
+const Header = (props) => <h2>{props.course}</h2>
 
 const Content = ({parts}) => (
   <div>
@@ -15,7 +15,7 @@ const Part = (props) => (
   </p>
 )
 
-const Total = (props) => <p><b>Total of {props.total} exercises</b> </p>
+const Total = (props) => <p><b>Total of {props.total} exercises</b></p>
 
 const Course = ({course}) => {
   const {name, parts} = course
@@ -35,29 +35,57 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }    
-    ]
-  }
-
-  return <Course course={course} />
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => <Course key={todoCounter++} course={course} />)}
+    </div>
+  )
 }
 
 
