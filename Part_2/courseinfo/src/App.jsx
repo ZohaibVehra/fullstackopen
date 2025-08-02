0df4b573e81aeaa896c48aae5864e9e5739f0955
Zohaib <zohaibv1999@gmail.com>
2025-08-01 23:10:29 -0400

    2.5

--- Part_2/courseinfo/src/App.jsx
+++ Part_2/courseinfo/src/App.jsx
@@ -1,38 +1,4 @@
-let todoCounter = 1;
-
-
-const Header = (props) => <h2>{props.course}</h2>
-
-const Content = ({parts}) => (
-  <div>
-    {parts.map((part) => <Part key={todoCounter++} part={part} /> )}
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => <p><b>Total of {props.total} exercises</b></p>
-
-const Course = ({course}) => {
-  const {name, parts} = course
-  
-
-
-  return(
-    <div>
-      <Header course={name} />
-
-      <Content parts={parts} />
-
-      <Total  total={parts.reduce((acc, curr) => acc+curr.exercises,0 )} />
-    </div>
-  )
-  
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
@@ -83,7 +49,7 @@ const App = () => {
   return (
     <div>
       <h1>Web development curriculum</h1>
-      {courses.map((course) => <Course key={todoCounter++} course={course} />)}
+      {courses.map((course) => <Course key={course.id} course={course} />)}
     </div>
   )
 }
---
+++ Part_2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,36 @@
+
+
+const Header = (props) => <h2>{props.course}</h2>
+
+const Content = ({parts}) => (
+  <div>
+    {parts.map((part) => <Part key={part.id} part={part} /> )}
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p><b>Total of {props.total} exercises</b></p>
+
+const Course = ({course}) => {
+  const {name, parts} = course
+  
+
+
+  return(
+    <div>
+      <Header course={name} />
+
+      <Content parts={parts} />
+
+      <Total  total={parts.reduce((acc, curr) => acc+curr.exercises,0 )} />
+    </div>
+  )
+  
+}
+
+export default Course
