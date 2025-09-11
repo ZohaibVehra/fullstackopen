ed4d883580ddfff9e669b6ec8f76850eb62598f0
Zohaib <zohaibv1999@gmail.com>
2025-09-11 12:10:18 -0400

    4.22

--- Part_4/controllers/blog.js
+++ Part_4/controllers/blog.js
@@ -1,7 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,14 +8,10 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', userExtractor, async (request, response) => {
   const { title, url, likes } = request.body
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-  if (!decodedToken.id) return response.status(401).json({ error: 'token invalid' })
-
-  const user = await User.findById(decodedToken.id)
+  const user = request.user
 
   const blog = new Blog({
     title,
@@ -34,14 +29,14 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+  const user = request.user
 
-  if (!decodedToken.id) return response.status(401).json({ error: 'token invalid' })
+  if (!user._id) return response.status(401).json({ error: 'token invalid' })
 
   const blogToDelete = await Blog.findById(request.params.id)
 
-  if(decodedToken.id.toString() !== blogToDelete.user.toString()){
+  if(user._id.toString() !== blogToDelete.user.toString()){
     return response.status(403).send({ error: 'blogs may only be deleted by user who owns blog' })
   }
 
