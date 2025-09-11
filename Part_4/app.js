f44737bb673dbfd201747ab8c93b91d1f309e47a
Zohaib <zohaibv1999@gmail.com>
2025-09-11 10:45:11 -0400

    4.15

--- Part_4/app.js
+++ Part_4/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -15,6 +16,7 @@ mongoose.connect(config.URL)
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ Part_4/controllers/users.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  if(!password) return response.status(400).send({ error: 'password required' })
+  const passwordHash = await bcrypt.hash(password, 10)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash
+  })
+
+  const savedUser = await user.save()
+  response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', { title: 1, url: 1, likes: 1 })
+  response.json(users)
+})
+
+
+module.exports = usersRouter
