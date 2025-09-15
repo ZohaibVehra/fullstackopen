45af5e3cf254ca610e88d24d4cf81880ad905a71
Zohaib <zohaibv1999@gmail.com>
2025-09-15 10:30:38 -0400

    5.14

--- Part_5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part_5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 
 test('blog renders title and author but not url or likes', async () => {
@@ -26,5 +27,35 @@ test('blog renders title and author but not url or likes', async () => {
   expect(screen.getByText('abcs of math', { exact: false })).toBeVisible()
   expect(screen.getByText('likes 13')).not.toBeVisible()
   expect(screen.getByText('url.com')).not.toBeVisible()
+
+})
+
+
+test('clicking view button shows url and likes', async () => {
+  const blog = {
+    title: 'abcs of math',
+    author: 'bobross',
+    url: 'url.com',
+    likes: 13,
+    user: {
+      username: 'test user username'
+    }
+  }
+  const userForBlog = {
+    username: 'test username',
+    id: 'test id',
+    name: 'test name'
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} delblog={mockHandler} user={userForBlog}/>)
+  screen.debug()
+
+  const button = screen.getByText('view')
+  const user = userEvent.setup()
+  await user.click(button)
+  expect(screen.getByText('likes 13')).toBeVisible()
+  expect(screen.getByText('url.com')).toBeVisible()
 
 })
