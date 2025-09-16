e6497ed6fda4ad6c53c5c488a272b5312a6f4bf1
Zohaib <zohaibv1999@gmail.com>
2025-09-16 16:35:49 -0400

    5.17

---
+++ Part_5/e2e/tests/blog.spec.js
@@ -0,0 +1,16 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5174')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    const locator = page.getByText('login')
+    await expect(page.getByText('Log in to application')).toBeVisible()
+    await expect(page.getByText('username')).toBeVisible()
+    await expect(page.getByText('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
+  })
+  
+})
